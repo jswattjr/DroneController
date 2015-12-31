83ad7bdc38d94d124c07c565cec80e9237bf64de
jswattjr <jswattjr@gmail.com>
2015-12-31 01:13:09 +0000

    implementing dronemanager

---
+++ DroneManager/Models/Drone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Interfaces;
+using DataAccessLibrary.Models;
+using DroneConnection;
+
+namespace DroneManager.Models
+{
+    public class Drone : DroneEntity
+    {
+        public Drone(DroneEntity entity)
+        {
+            this.copy(entity);
+        }
+
+        public Drone()
+        {
+
+        }
+
+        public DroneLink connection { get; set; }
+    }
+}
