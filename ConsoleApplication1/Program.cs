bc942ff4b6be68433b00dde08bc12e7e17211e88
jswattjr <jswattjr@gmail.com>
2016-01-09 03:03:18 +0000

    Adding DBup project for db initialization

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbUp;
+using System.Reflection;
+
+namespace DBupConsole
+{
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            var connectionString =
+       args.FirstOrDefault()
+       ?? "Server=localhost;Trusted_connection=true";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
