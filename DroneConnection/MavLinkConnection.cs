3c7929911a6706b567533532829bf6c60bb476d5
jswattjr <jswattjr@gmail.com>
2016-01-30 00:28:27 +0000

    trimming logs

--- DroneConnection/MavLinkConnection.cs
+++ DroneConnection/MavLinkConnection.cs
@@ -172,7 +172,7 @@ namespace DroneConnection
                     continue;
                 }
 
-                logger.Debug("{1} Message read from target system: {0}, component id {3}, of type {2}", message.sysid, message.messid, message.getMessageType().ToString(), message.compid);
+                logger.Trace("{1} Message read from target system: {0}, component id {3}, of type {2}", message.sysid, message.messid, message.getMessageType().ToString(), message.compid);
                 this.systemId = message.sysid;
                 this.componentId = message.compid;
                 this.postMessage(message);
